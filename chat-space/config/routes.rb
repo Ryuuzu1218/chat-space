45c0c5233ecc0dc0ad709f4f5b55f921a34fed97
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2019-12-25 13:09:23 +0900

    devise
    deviseの機能をつけました。

--- chat-space/config/routes.rb
+++ chat-space/config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   devise_for :users
+  resources :users, only:  [:edit, :update]
 root "messages#index"
->>>>>>> Stashed changes
-=======
-root "messages#index"
->>>>>>> 7ad3877bcb541e81497889f0a63671b73c1b1fd3
 end
