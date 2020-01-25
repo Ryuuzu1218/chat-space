a4d80aa57d477c96398af1a8088160cb4ea6383e
Ryuuzu1218 <entertain-yourself@docomo.ne.jp>
2020-01-25 15:57:09 +0900

    1/25 15:55
    自動更新機能を追加しました。

--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -1,37 +1,45 @@
 $(function(){
-     function buildHTML (message){
-      var addImage = '';
-        if(message.image){
+
+  function buildHTML (message){
+    var addImage = '';
+      if(message.body && message.image){
+        var addImage = `<img src="${message.image}">`;
+          var html = 
+          `<div class="main-chat__message-list__box__one__downer__body">`+
+            message.body +
+          `</div>`+
+          `<div class="main-chat__message-list__box__one__downer__image">`+
+                   addImage +
+                 `</div>`
+        }   else if(message.body)  {
+          var html = `<div class="main-chat__message-list__box__one__upper" data-message-id=` + message.id + `>` +
+                    `<div class="main-chat__message-list__box__one__upper__name">`+
+                      message.user_name +
+                    `</div>`+
+                    `<div class="main-chat__message-list__box__one__upper__datetime">`+
+                      message.created_at +
+                    `</div>`+
+                  `</div>`+
+                    `<div class="main-chat__message-list__box__one__downer__body">`+
+                      message.body +
+                    `</div>`
+        }   else if(message.image)  {
           var addImage = `<img src="${message.image}">`;
-            var html = `<div class="main-chat__message-list__box__upper">
-                          <div class="main-chat__message-list__box__upper__name">
-                          ${message.user_name}
-                          </div>
-                          <div class="main-chat__message-list__box__upper__datetime">
-                          ${message.created_at}
-                          </div>
-                        </div>
-                          <div class="main-chat__message-list__box__downer__body">
-                            ${message.body}
-                          </div>
-                          <div class="main-chat__message-list__box__downer__image">
-                            ${addImage}
-                          </div>`
-            return html;
-        }   else  {
-           var html=`<div class="main-chat__message-list__box__upper">
-                      <div class="main-chat__message-list__box__upper__name">
-                        ${message.user_name}
-                      </div>
-                      <div class="main-chat__message-list__box__upper__datetime">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                      <div class="main-chat__message-list__box__downer__body">
-                        ${message.body}
-                      </div>`
-            return html
-                      }}
+          var html=
+                   `<div class="main-chat__message-list__box__one__upper" data-message-id=` + message.id + `>` +
+                     `<div class="main-chat__message-list__box__one__upper__name">`+
+                       message.user_name +
+                     `</div>`+
+                     `<div class="main-chat__message-list__box__one__upper__datetime">`+
+                       message.created_at +
+                     `</div>`+
+                   `</div>`+
+                   `<div class="main-chat__message-list__box__one__downer__image">`+
+                   addImage +
+                 `</div>`
+                  }
+                    return html;
+                  }
 $('#new_message').on('submit',function(e){
     e.preventDefault()
     var formData=new FormData(this)
@@ -46,7 +54,7 @@ $('#new_message').on('submit',function(e){
     })
     .done(function(data){
       var html = buildHTML(data)
-      $('.main-chat__message-list').append(html)
+      $('.main-chat__message-list__box').append(html)
       $('form')[0].reset()
       $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
       $('.send').prop('disabled',false)
@@ -55,4 +63,38 @@ $('#new_message').on('submit',function(e){
       alert("メッセージ送信に失敗しました")
     })
   })  
+  var reloadMessages=function(){
+    last_message_id = $('.main-chat__message-list__box__one__upper:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url:"api/messages",
+      type:'get',
+      dataType:'json',
+      data:{id: last_message_id}
+    })
+    .done(function(message){
+      if (message.length !== 0) {
+      var insertHTML=''
+      console.log(message)
+      
+      //console.log(buildHTML)
+      $.each(message, function(i, message){
+        insertHTML += buildHTML(message)
+      })
+      
+      console.log (insertHTML)
+
+      $('.main-chat__message-list__box').append(insertHTML)
+      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      $("#new_message")[0].reset();
+      $(".form__submit").prop("disabled", false);
+    }
+    })
+    .fail(function(){
+      console.log('自動更新でエラー')
+    })
+  }
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages,7000)
+  }
 })
--- chat-space/app/views/messages/create.json.jbuilder
+++ chat-space/app/views/messages/create.json.jbuilder
@@ -2,4 +2,5 @@ json.created_at @message.created_at.to_s
 json.body @message.body
 json.image @message.image.url
 json.user_name @message.user.name
+json.id @message.id
 
